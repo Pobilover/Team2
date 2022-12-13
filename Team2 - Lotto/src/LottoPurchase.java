bf620a042c24fd47a74abb09f9616b27b4952b4c
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-13 16:44:42 +0900

    구매창 오류 수정

--- Team2 - Lotto/src/LottoPurchase.java
+++ Team2 - Lotto/src/LottoPurchase.java
@@ -316,14 +316,14 @@ class Purchase extends JDialog implements MouseListener, ActionListener, ItemLis
 			String name = "num" + (index + 1) + ".png";
 			if (lottoNumsIcon[index] && nowGameCounters[nowGame] <= 6) {
 				lblNumbers.get(index).setIcon(getIcon("numbers/" + name, 30, 30)); 
-				lottoNumsIcon[index + 1] = false;
+				lottoNumsIcon[index] = false;
 				nowGameCounters[nowGame]--;
-				inputNumbers.remove(inputNumbers.indexOf(index + 1));
+				inputNumbers.remove(inputNumbers.indexOf(index));
 			} else if (!lottoNumsIcon[index] && nowGameCounters[nowGame] < 6) {
 				lblNumbers.get(index).setIcon(getIcon("afterNumbers/" + name, 30, 30));
 				lottoNumsIcon[index] = true;
 				nowGameCounters[nowGame]++;
-				inputNumbers.add(index + 1);
+				inputNumbers.add(index);
 			} else if (!lottoNumsIcon[index] && nowGameCounters[nowGame] >= 6) {
 				JOptionPane.showMessageDialog(null, "최대 6개의 숫자를 선택할 수 있습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
 			}
